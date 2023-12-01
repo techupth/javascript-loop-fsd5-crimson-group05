18efdc711910c4ee92311b6c8cd0e54df40ed973
devsomsak <devrleang@gmail.com>
2023-12-01 15:39:03 +0700

    fix:ex-6.js js-loop

--- ex-6.js
+++ ex-6.js
@@ -3,8 +3,9 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-for (let i = companyName.length - 1; i > -1; i--) {
-  reversedCompanyName += companyName[i];
+for (let i = 0; i < companyName.length ;i++) {
+  reversedCompanyName = companyName[i]+reversedCompanyName;
 }
 
 console.log(reversedCompanyName);
+add
